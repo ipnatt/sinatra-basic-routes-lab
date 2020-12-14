f5c0b2a509e3935994ff6f96969fe4f2d83d40ff
ipnatt <ipnatt@gmail.com>
2020-12-14 11:54:24 -0800

    finished

--- app.rb
+++ app.rb
@@ -1,4 +1,25 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Irv"
+    end
+
+    get '/hometown' do
+        "My hometown is San Jose, CA"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is I'm not really sure"
+    end
+
+
+
+
+
 end
